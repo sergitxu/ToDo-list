877fa132dd552fb7854e6ede4f27305d4ba6e1c7
sergitxu <sergiortiz@gmail.com>
2023-11-29 11:16:41 +0100

    Connect to MongoDB

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
+
+const MONGO = require('./secrets');
+
 const express = require("express");
+const mongoose = require("mongoose");
 const taskController = require("./controllers/taskController");
 
 const app = express();
@@ -13,3 +17,35 @@ app.get("/tasks", taskController.getAllTasks);
 app.post("/tasks", taskController.createTask);
 app.put("/tasks/:id", taskController.updateTask);
 app.delete("/tasks/:id", taskController.deleteTask);
+
+mongoose.connect(
+    MONGO.connectionString,
+    {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+    }
+);
+
+const taskSchema = {
+    title: { type: String, required: true },
+    description: String,
+    dueDate: Date,
+    completed: Boolean,
+    createdAt: { type: Date, default: Date.now },
+    updatedAt: Date,
+    deletedAt: Date
+}
+
+const Task = mongoose.model('Task', taskSchema);
+
+const task = new Task({
+    title: 'Task 2',
+    description: 'Description 2',
+    dueDate: new Date(),
+    completed: false
+});
+task.save()
+    .then(
+        () => console.log("One task added"),
+        (err) => console.log(err)
+    );
